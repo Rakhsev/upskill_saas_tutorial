dbff058e4a6cedda674f08fd5e6f661bdbc45763
Yoann Bouet <yoann.bouet60@orange.fr>
2020-06-18 18:09:24 +0000

    Sorting things out

--- Gemfile
+++ Gemfile
@@ -17,6 +17,8 @@ gem 'jquery-rails', '4.1.1'
 gem 'turbolinks', '5.0.0'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '2.5.0'
+# Sendgrif mailer
+gem 'sendgrid-ruby'
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '3.0'
 # Use ActiveModel has_secure_password
--- config/environment.rb
+++ config/environment.rb
@@ -3,13 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-ActionMailer::Base.smtp_settings = {
-  :port           => ENV['TRUSTIFI_SMTP_PORT'],
-  :address        => ENV['TRUSTIFI_SMTP_SERVER'],
-  :user_name      => ENV['TRUSTIFI_SMTP_LOGIN'],
-  :password       => ENV['TRUSTIFI_SMTP_PASSWORD'],
-  :domain         => 'desolate-journey-66390.herokuapp.com',
-  :authentication => :plain,
-}
-ActionMailer::Base.delivery_method = :smtp
